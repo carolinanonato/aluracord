a965dcb80cf3974270d3e067fdb3ff1fc55f1231
Carolina Nonato <csnonato@gmail.com>
2022-01-27 22:27:02 -0500

    chat page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "581a9e107c4c1b1d"; }
+/******/ 		__webpack_require__.h = function() { return "7c708c1ca470ec5b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
